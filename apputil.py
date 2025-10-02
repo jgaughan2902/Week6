12e8cffda7c9110177583c9a5d814087f8c9e76d
jgaughan2902 <jdgaugha@iu.edu>
2025-10-02 15:32:52 +0000

    Exercise 1

--- apputil.py
+++ apputil.py
@@ -1,1 +1,14 @@
-# your code here ...
+# Exercise 1
+
+class Genius:
+    """
+    A class that stores the access token.
+    """
+    def __init__(self, access_token):
+        """
+        Initializes Genius with the access_token.
+
+        Parameters:
+        access_token (str): API access token.
+        """
+        self.access_token = access_token
