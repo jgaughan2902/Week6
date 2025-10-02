29bacba6630f88bb2db8d3088fff804868a0f1da
jgaughan2902 <jdgaugha@iu.edu>
2025-10-02 15:48:30 +0000

    Initial Exercise 2

--- apputil.py
+++ apputil.py
@@ -12,3 +12,19 @@ class Genius:
         access_token (str): API access token.
         """
         self.access_token = access_token
+
+# Exercise 2
+
+import requests
+
+class Genius:
+    def get_artist(self, search_term):
+
+        search_term = search_term
+        genius_url = "http://api.genius.com/search?q={search_term}" \
+        "&access_token={ACCESS_TOKEN}"
+
+        resp = requests.get(genius_url, headers = {"Authorization":
+                            "Bearer " + ACCESS_TOKEN})
+        json_data = resp.json()
+
